e7d07ac875ff61d64c93a598c3459dc4f4423fad
jygonzalez <jorge.gonzalezalcoce@ucalgary.ca>
2024-09-10 20:13:47 -0600

    Add report file

--- dictionaryList.cpp
+++ dictionaryList.cpp
@@ -1,8 +1,10 @@
-// lookuptable.cpp
-
-// Lab 1 - Exercise A
- 
-// Completed by:
+/**
+ *  File Name: dictionaryList.cpp
+ *  Assignment: ENSF 614 Fall 2024 - Lab 1 Exercise
+ *  Created by: Mahmood Moussavi
+ *  Completed by: Yael Gonzalez
+ *  Submission Date: September 18, 2024
+ */
 
 #include <assert.h>
 #include <iostream>
@@ -12,24 +14,25 @@
 
 using namespace std;
 
-Node::Node(const Key& keyA, const Datum& datumA, Node *nextA)
-  : keyM(keyA), datumM(datumA), nextM(nextA)
+Node::Node(const Key &keyA, const Datum &datumA, Node *nextA)
+    : keyM(keyA), datumM(datumA), nextM(nextA)
 {
 }
 
 DictionaryList::DictionaryList()
-  : sizeM(0), headM(0), cursorM(0)
+    : sizeM(0), headM(0), cursorM(0)
 {
 }
 
-DictionaryList::DictionaryList(const DictionaryList& source)
+DictionaryList::DictionaryList(const DictionaryList &source)
 {
   copy(source);
 }
 
-DictionaryList& DictionaryList::operator =(const DictionaryList& rhs)
+DictionaryList &DictionaryList::operator=(const DictionaryList &rhs)
 {
-  if (this != &rhs) {
+  if (this != &rhs)
+  {
     destroy();
     copy(rhs);
   }
@@ -51,128 +54,129 @@ int DictionaryList::cursor_ok() const
   return cursorM != 0;
 }
 
-const Key& DictionaryList::cursor_key() const
+const Key &DictionaryList::cursor_key() const
 {
   assert(cursor_ok());
   return cursorM->keyM;
 }
 
-const Datum& DictionaryList::cursor_datum() const
+const Datum &DictionaryList::cursor_datum() const
 {
   assert(cursor_ok());
   return cursorM->datumM;
 }
 
-void DictionaryList::insert(const int& keyA, const Mystring& datumA)
+void DictionaryList::insert(const int &keyA, const Mystring &datumA)
 {
-  // Add new node at head?                                                                                  
-  if (headM == 0 || keyA < headM->keyM) {
+  // Add new node at head?
+  if (headM == 0 || keyA < headM->keyM)
+  {
     headM = new Node(keyA, datumA, headM);
     sizeM++;
   }
-	
-  // Overwrite datum at head?                                                                               
+
+  // Overwrite datum at head?
   else if (keyA == headM->keyM)
     headM->datumM = datumA;
-	
-  // Have to search ...                                                                                     
-  else {
-      
-    //POINT ONE
-		
-    // if key is found in list, just overwrite data;                                                        
-    for (Node *p = headM; p !=0; p = p->nextM)
-		{
-			if(keyA == p->keyM)
-			{
-				p->datumM = datumA;
-				return;
-			}
-		}
-		
-    //OK, find place to insert new node ...                                                                 
-    Node *p = headM ->nextM;
+
+  // Have to search ...
+  else
+  {
+
+    // POINT ONE
+
+    // if key is found in list, just overwrite data;
+    for (Node *p = headM; p != 0; p = p->nextM)
+    {
+      if (keyA == p->keyM)
+      {
+        p->datumM = datumA;
+        return;
+      }
+    }
+
+    // OK, find place to insert new node ...
+    Node *p = headM->nextM;
     Node *prev = headM;
-		
-    while(p !=0 && keyA >p->keyM)
-		{
-			prev = p;
-			p = p->nextM;
-		}
-		
+
+    while (p != 0 && keyA > p->keyM)
+    {
+      prev = p;
+      p = p->nextM;
+    }
+
     prev->nextM = new Node(keyA, datumA, p);
     sizeM++;
   }
   cursorM = NULL;
-	
 }
 
-void DictionaryList::remove(const int& keyA)
+void DictionaryList::remove(const int &keyA)
 {
-    if (headM == 0 || keyA < headM -> keyM)
-        return;
-    
-    Node *doomed_node = 0;
-    
-    if (keyA == headM-> keyM) {
-        doomed_node = headM;
-        headM = headM->nextM;
-        
-        // POINT TWO
+  if (headM == 0 || keyA < headM->keyM)
+    return;
+
+  Node *doomed_node = 0;
+
+  if (keyA == headM->keyM)
+  {
+    doomed_node = headM;
+    headM = headM->nextM;
+
+    // POINT TWO
+  }
+  else
+  {
+    Node *before = headM;
+    Node *maybe_doomed = headM->nextM;
+    while (maybe_doomed != 0 && keyA > maybe_doomed->keyM)
+    {
+      before = maybe_doomed;
+      maybe_doomed = maybe_doomed->nextM;
     }
-    else {
-        Node *before = headM;
-        Node *maybe_doomed = headM->nextM;
-        while(maybe_doomed != 0 && keyA > maybe_doomed-> keyM) {
-            before = maybe_doomed;
-            maybe_doomed = maybe_doomed->nextM;
-        }
-        
-        if (maybe_doomed != 0 && maybe_doomed->keyM == keyA) {
-            doomed_node = maybe_doomed;
-            before->nextM = maybe_doomed->nextM;
-        }
-        
-        
-    } 
-    if(doomed_node == cursorM)
-        cursorM = 0;
-    
-    delete doomed_node;           // Does nothing if doomed_node == 0.
-    sizeM--;
+
+    if (maybe_doomed != 0 && maybe_doomed->keyM == keyA)
+    {
+      doomed_node = maybe_doomed;
+      before->nextM = maybe_doomed->nextM;
+    }
+  }
+  if (doomed_node == cursorM)
+    cursorM = 0;
+
+  delete doomed_node; // Does nothing if doomed_node == 0.
+  sizeM--;
 }
 
 void DictionaryList::go_to_first()
 {
-    cursorM = headM;
+  cursorM = headM;
 }
 
 void DictionaryList::step_fwd()
 {
-    assert(cursor_ok());
-    cursorM = cursorM->nextM;
+  assert(cursor_ok());
+  cursorM = cursorM->nextM;
 }
 
 void DictionaryList::make_empty()
 {
-    destroy();
-    sizeM = 0;
-    cursorM = 0;
+  destroy();
+  sizeM = 0;
+  cursorM = 0;
 }
-
 
 // The following function are supposed to be completed by the stuents, as part
 // of the exercise B part II. the given fucntion are in fact place-holders for
 // find, destroy and copy, in order to allow successful linking when you're
 // testing insert and remove. Replace them with the definitions that work.
 
-void DictionaryList::find(const Key& keyA)
+void DictionaryList::find(const Key &keyA)
 {
   cout << "\nDon't know how to find " << keyA << " (or any other key).\n";
   cout << "... so exit is being called.\n";
   exit(1);
 }
-
 
 void DictionaryList::destroy()
 {
@@ -181,13 +185,10 @@ void DictionaryList::destroy()
   headM = 0;
 }
 
-
-void DictionaryList::copy(const DictionaryList& source)
+void DictionaryList::copy(const DictionaryList &source)
 {
-  
+
   cout << "\nDictionaryList::copy is not implemented properly,\n"
        << "so the program is calling exit.\n";
   exit(1);
 }
-
-
